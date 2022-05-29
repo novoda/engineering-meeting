45d8e4371d2e4a8b989781a6cc84844294806c2e
Juan Soriano <juanky@novoda.com>
2022-05-29 16:40:49 -0500

    Uses HashRouter in order to fix refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import EngineeringMeeting from "./engineering-meeting/engineering-meeting"
 import Landing from "./landing"
 import "./styles/index.css"
 
 ReactDOM.render(
-   <BrowserRouter>
+   <HashRouter>
       <Routes>
-         <Route path="/engineering-meeting" element={<Landing />} />
+         <Route path="/" element={<Landing />} />
          <Route path="/randomiser" element={<EngineeringMeeting />} />
       </Routes>
-   </BrowserRouter>,
+   </HashRouter>,
    document.getElementById("root")
 )

1637e3d93c0dd1697fb96c6fe46e169819814e5f
Jozef Celuch <jozef@novoda.com>
2022-09-20 11:57:53 +0200

    Create ui state with content loading and error

--- src/engineering-meeting/engineering-meeting.tsx
+++ src/engineering-meeting/engineering-meeting.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { toast, ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { BuildingBlock } from "./building-block"
-import {} from "./extensions"
+import { } from "./extensions"
 import { MeetingRandomiser } from "./meeting-randomiser"
 import { ScreenshotTaker } from "./screenshot-taker"
 import EngineeringMeetingSketch from "./sketch/sketch"
@@ -11,20 +11,14 @@ import "./styles/engineering-meeting.css"
 
 export default function EngineeringMeeting() {
    const randomiser = MeetingRandomiser.create()
-   const initialMeeting = randomiser.randomise()
    const screenshotTaker = new ScreenshotTaker()
-   const [blocks, setBlocks] = useState<BuildingBlock[]>(initialMeeting.blocks)
-   const [name, setName] = useState<string>(initialMeeting.name)
-   const [duration, setDuration] = useState<string>(initialMeeting.duration)
-   const [date, setDate] = useState<string>(initialMeeting.generatedDate)
+   const [uiState, setState] = useState<UiState>(randomContent(randomiser))
 
    async function randomise() {
+      setState(new Loading())
       await timeout(3000) // TODO remove fake delay when real time-consuming action is added
       const meeting = randomiser.randomise()
-      setName(meeting.name)
-      setBlocks(meeting.blocks)
-      setDuration(meeting.duration)
-      setDate(meeting.generatedDate)
+      setState(randomContent(randomiser))
    }
 
    async function structureToClipboard() {
@@ -43,27 +37,41 @@ export default function EngineeringMeeting() {
       )
    }
 
+   if (uiState instanceof Content) {
+      return content(uiState, structureToClipboard, randomise)
+   } else if (uiState instanceof Loading) {
+      return loading()
+   } else {
+      return error()
+   }
+}
+
+function content(
+   uiState: Content,
+   copyToClipboard: () => Promise<void>,
+   randomise: () => Promise<void>
+) {
    return (
-      <SketchProvider.Provider value={blocks}>
+      <SketchProvider.Provider value={uiState.blocks}>
          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, viewport-fit=cover" />
          <a href="https://github.com/novoda/engineering-meeting">
             <img className="logo" src={`${process.env.PUBLIC_URL}/images/novoda.png`} alt="Novoda" exclude-from-screenshot="yes" />
          </a>
          <div id="meeting" className="engineeringMeeting">
             <div className="sketch-view">
-               <div onClick={structureToClipboard}>
+               <div onClick={copyToClipboard}>
                   <img className="click" src={`${process.env.PUBLIC_URL}/images/click-here-to-copy.png`} alt="Copy" exclude-from-screenshot="yes" />
                   <EngineeringMeetingSketch />
                </div>
-               <b><span id="date-generated">{date}</span></b>
+               <b><span id="date-generated">{uiState.date}</span></b>
                <button className="randomiseButton" onClick={randomise} exclude-from-screenshot="yes">
                   <b>Randomise</b>
                </button>
                <ToastContainer position="top-center" autoClose={1000} hideProgressBar={true} />
             </div>
             <div className="meeting-structure">
-               <h2 className="meeting-name">{name}</h2>
-               {[...blocks].reverse().map((block) => (
+               <h2 className="meeting-name">{uiState.name}</h2>
+               {uiState.blocks.reverse().map((block) => (
                   <section key={block.id} className="block">
                      <img className="block-image" src={block.imagePath} alt={block.name} />
                      <div className="block-details">
@@ -81,7 +89,7 @@ export default function EngineeringMeeting() {
                   </section>
                ))}
                <p className="meeting-duration">
-                  <b>Estimated duration:</b> {duration}
+                  <b>Estimated duration:</b> {uiState.duration}
                </p>
             </div>
          </div>
@@ -89,6 +97,72 @@ export default function EngineeringMeeting() {
    )
 }
 
+function loading() {
+   return (
+      <body>
+         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, viewport-fit=cover" />
+         <div className="loading-content">
+            <h2 className="loading">Loading</h2>
+         </div>
+      </body>
+   )
+}
+
+function error() {
+   return (
+      <body>
+         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, viewport-fit=cover" />
+         <div className="error-content">
+            <h2 className="error">Error</h2>
+         </div>
+      </body>)
+}
+
 function timeout(ms: number) {
    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function randomContent(randomiser: MeetingRandomiser): Content {
+   const meeting = randomiser.randomise()
+   return new Content(
+      meeting.blocks,
+      meeting.name,
+      meeting.duration,
+      meeting.generatedDate
+   )
+}
+
+abstract class UiState { }
+
+class Loading extends UiState { }
+
+class Error extends UiState {
+   message: string
+
+   constructor(
+      message: string,
+   ) {
+      super()
+      this.message = message
+   }
+}
+
+class Content extends UiState {
+   blocks: BuildingBlock[]
+   name: string
+   duration: string
+   date: string
+
+   constructor(
+      blocks: BuildingBlock[],
+      name: string,
+      duration: string,
+      date: string
+   ) {
+      super()
+      this.blocks = blocks
+      this.name = name
+      this.duration = duration
+      this.date = date
+   }
 }

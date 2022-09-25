436aac7c5702f11f5355a2af2623670a76894d35
Juan Soriano <juanky@novoda.com>
2022-09-25 16:09:02 -0500

    Cleanup weird terms from name generation

--- src/engineering-meeting/meeting-randomiser.ts
+++ src/engineering-meeting/meeting-randomiser.ts
@@ -80,23 +80,23 @@ function getDateTime(): string {
 }
 
 class NameRandomiser {
-   private nouns = ['Russian Rocket', 'Egg Salad', 'Yale-鸭力', 'Ham Sandwich', 'Chicken McNugget', 'Club Turkey', 'Yellow Hat', 'Isolation', 'Wiggly Finger', 'Chi-Squared', 'Unicorn', 'Holographic', 'Extract',
-      'Fish Guts', 'Bakersfield', 'Orange Juice', 'Scavenger', 'Colonization', 'Probabilistic', 'Gyroscopic', 'Cognition', 'Collaboration', 'Geology', 'Conjugate', 'Paradise', 'Comic Book',
-      'Comic Book Store', 'Public Restroom', 'Onion', 'Boyfriend', 'Girlfriend', 'Happy Marriage', 'Roommate', 'Hello', 'Bacon', 'Pork Chop', 'Nerdvana', 'Planetary', 'Aquatic', 'Dumpster Dumpling',
-      'Dumpling', 'Take Out', 'Badger', 'Cali', 'Awkward Angle', 'Baby', 'Smarties', 'Junk Food', 'French Fry', 'Animalcule', 'Molecule', 'Cornfield', 'Train', 'Train Collector\'s', 'Pyramidal Train',
-      'Christmas Tree', 'Self-Roast', 'Submarine', 'Navy Dolphin', 'Coiling Snake', 'Broken Record', 'Constellation', 'Star', 'Terminator', 'Procrastination', 'Dancing',
+   private nouns = ['Egg Salad', 'Ham Sandwich', 'Chicken McNugget', 'Club Turkey', 'Yellow Hat', 'Isolation', 'Wiggly Finger', 'Chi-Squared', 'Unicorn', 'Holographic', 'Extract',
+      'Fish Guts', 'Bakersfield', 'Orange Juice', 'Scavenger', 'Colonization', 'Probabilistic', 'Gyroscopic', 'Cognition', 'Collaboration', 'Conjugate', 'Paradise', 'Comic Book',
+      'Comic Book Store', 'Public Restroom', 'Onion', 'Boyfriend', 'Girlfriend', 'Happy Marriage', 'Roommate', 'Bacon', 'Pork Chop', 'Nerdvana', 'Planetary', 'Aquatic', 'Dumpster Dumpling',
+      'Dumpling', 'Take Out', 'Badger', 'Cali', 'Awkward Angle', 'Baby', 'Smarties', 'Junk Food', 'French Fry', 'Animalcule', 'Cornfield', 'Train', 'Train Collector\'s', 'Pyramidal Train',
+      'Christmas Tree', 'Self-Roast', 'Submarine', 'Navy Dolphin', 'Coiling Snake', 'Broken Record', 'Constellation', 'Star', 'Procrastination', 'Dancing',
       'Driving', 'Periodic', 'Newspaper', 'Jerusalem', 'Buddha', 'Complicated', 'Simplistic', 'Watermelon', 'Piñata', 'Party Pooper', 'Chess', '3D Chess', '4D Chess', 'Avocado', 'Pizza Man',
-      'Pizza Woman', 'Pizza', 'Banana', 'Yellow Minion', 'Despicable Minion', '4th Grade', '5th Grade', '13th Grade', 'Undergraduate', 'Cabbage, Kale, Lettuce', 'Something', 'Dark Matter', 'World War 0',
-      'Anti-Matter', 'Electron', 'Proton', 'Muon', 'Ion', 'Isotope', 'Radioactivity', 'Toothpaste', 'Grasshopper', 'Ladybug', 'Mosquito', 'Quesadilla', 'Taco', 'Burrito', 'Bread Crumb', 'Pancake Stack', 'Whip Cream',
-      'Christmas', 'Ionic', 'Ionic Compound', 'Covalent', 'Covalent Compound', 'Romance', 'Midnight', 'Monopolar', 'Troll', 'Hamburger', 'Spaghetti', 'Chicken Noodle Soup', 'Plankton', 'Ironic Ionic', 'Venus', 'Mars',
-      'Date Night', 'Cooking Night', 'Game Night', 'Friendship', 'Fiendship', 'Status Quo', 'Bono', 'Raider\'s', 'Hawking', 'Spock', 'Euclid', 'Gauss', 'Eisenstein', 'Einstein', 'Feynmann', 'Lewis', 'Gru',
-      'Ana', 'Jozef', 'Frank', 'Diogo', 'Michal', 'Carl', 'Pablo', 'Juan', 'Giuseppe', 'Richard', 'Ben', 'Januz', 'Laith', 'Baris', 'Ken', 'Lukasz', 'Laurie', 'Adam', 'Ataul', 'Paul', 'Xavi', 'Antonio',
+      'Pizza Woman', 'Pizza', 'Banana', 'Yellow Minion', 'Despicable Minion', 'Cabbage, Kale, Lettuce', 'Something', 'Dark Matter',
+      'Anti-Matter', 'Toothpaste', 'Grasshopper', 'Ladybug', 'Mosquito', 'Quesadilla', 'Taco', 'Burrito', 'Bread Crumb', 'Pancake Stack', 'Whip Cream',
+      'Christmas', 'Romance', 'Midnight', 'Monopolar', 'Troll', 'Hamburger', 'Spaghetti', 'Chicken Noodle Soup', 'Plankton', 'Ironic Ionic', 'Venus', 'Mars',
+      'Date Night', 'Cooking Night', 'Game Night', 'Friendship', 'Status Quo', 'Bono', 'Raider\'s', 'Lewis\'s', 'Gru\'s', 'Moquito\'s', 'Persian Cat', 'Cat', 'Dog', 'Pug', 'Whale', 'Orange',
+      'Ana\'s', 'Jozef\'s', 'Frank\'s', 'Diogo\'s', 'Michal\'s', 'Carl\'s', 'Pablo\'s', 'Mina\'s', 'Bartosz\'s', 'Juan\'s', 'Giuseppe\'s', 'Richard\'s', 'Ben\'s', 'Janusz\'s', 'Laith\'s', 'Baris\'s', 'Ken\'s', 'Lukasz\'s', 'Laurie\'s', 'Sarah\'s', 'Adam\'s', 'Ataul\'s', 'Paul\'s', 'Xavi\'s', 'Antonio\'s',
       'Soundcloud', 'Channel 4', 'Hopin', 'Glovo', 'Vodafone', 'Tesco', 'Huddle', 'All4', 'Tantalum', 'Charlotte & Tiltbury', 'Blu', 'Depict', 'Body Coach']
-   private terms = ['Initiation', 'Equivalency', 'Equivalence Relation', 'Equivalence Solution', 'Solution', 'Test', 'Integration Test', 'Unit Test', 'UI Test', 'Automated Test', 'Maneuver', 'Acquisition', 'Formulation',
-      'Extension Function', 'Expansion', 'Factor', 'Transformation', 'Malfunction', 'Coefficient', 'Convergence', 'Divergence', 'Limit', 'Proximity Test', 'Minimization', 'Insufficiency', 'Estimation', 'Optimization',
-      'Permutation', 'Application', 'Synthesis', 'Path', 'Graph', 'Initiative', 'Effect', 'Problem', 'Instability', 'Stability Test', 'Paradox', 'Number', 'Anomaly', 'Quirk', 'Manipulation', 'Justification', 'Process',
-      'Triangulation', 'Algorithm', 'Program', 'Side Effect', 'Fallacy', 'Decoupling', 'Paradigm', 'Correlation', 'Measurement', 'Alternative', 'Simplex', 'Animation', 'Tensor', 'Dimension', 'Normalization', 'Renormalization',
-      'Experiment', 'Migration', 'Fragmentation', 'Deficiency', 'Displacement', 'Contamination', 'Isotope', 'Dissipation', 'Integration', 'Diversion', 'Approximation', 'Catalyst', 'Pull Request', 'Bug', 'Feature Request',
+   private terms = ['Solution', 'Test', 'Integration Test', 'Unit Test', 'UI Test', 'Automated Test', 'Kubernetes Cluster',
+      'Extension Function', 'Expansion', 'Factor', 'Transformation', 'Malfunction', 'Limit', 'Proximity Test', 'Minimization', 'Estimation', 'Optimization',
+      'Permutation', 'Application', 'Synthesis', 'Path', 'Graph', 'Initiative', 'Effect', 'Problem', 'Instability', 'Stability Test', 'Number', 'Anomaly', 'Manipulation', 'Justification', 'Process',
+      'Triangulation', 'Algorithm', 'Program', 'Side Effect', 'Decoupling', 'Paradigm', 'Correlation', 'Measurement', 'Alternative', 'Animation', 'Tensor', 'Dimension', 'Normalization',
+      'Experiment', 'Migration', 'Fragmentation', 'Integration', 'Pull Request', 'Bug', 'Feature Request',
       'Github Issue', 'Rebase', 'Bisection', 'Commit', 'Merge', 'Refactor', 'Monad', 'JIRA Ticket', 'Sprint', 'Retrospective', 'Ternary Operator', 'Switch', 'Sealed Class', 'Sealed Interface', 'Sealed Enum', 'Sealed Object',
       'Sealed Function', 'Sealed Method', 'Data Class', 'Interface', 'Enum', 'Object', 'Function', 'Method', 'Programming Language', 'Coroutine', 'Promise', 'Flow', 'Boolean', 'Integer', 'Double', 'String', 'JSON', 'XML', 'YAML',
       'Neural Network', 'For Loop', 'FlatMap', 'Script', 'Kata', 'Pair Programming Session', 'Arrow Function', 'Configuration File', 'Feature', 'Estimation', 'Spike', 'Memory Leak', 'Regular Expression', 'Arithmetic Operator',
@@ -105,7 +105,7 @@ class NameRandomiser {
       'Private Function', 'Public Function', 'Nested Class', 'Exception', 'Stack Trace', 'Syntax Error', 'Compiler Error', 'Runtime Error', 'Branch'
    ]
    private adjectives = ['Aquatic', 'Romantic', 'Funny', 'Futuristic', 'Big', 'Tiny', 'Small', 'Dummy', 'Fantastic', 'Complicated', 'Extraordinary', 'Toxic', 'Magnificient', 'Fantabulous', 'Weird', 'Expensive',
-      'Perfect', 'Calm', 'Senseless', 'Paranormal', 'Incredible', 'Accurate', 'Aggresive', 'Ancient', 'New', 'Bright', 'Colorful', 'Cute', 'Deep', 'Dark', 'Elegant', 'Fancy', 'Huge', 'Impossible', 'Important']
+      'Perfect', 'Calm', 'Senseless', 'Paranormal', 'Incredible', 'Accurate', 'Ancient', 'New', 'Bright', 'Colorful', 'Cute', 'Deep', 'Dark', 'Elegant', 'Fancy', 'Huge', 'Impossible', 'Important']
 
    randomise(): string {
       const adjective = Math.random() > 0.5 ? this.adjectives[Math.floor(Math.random() * this.adjectives.length)] : ''
